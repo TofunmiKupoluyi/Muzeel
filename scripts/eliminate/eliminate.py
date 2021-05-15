b0c9b95674d2d8e2d6e019c7007ad164fcdd795f
Tofunmi Kupoluyi <tofunmi@live.com>
2021-05-15 14:57:19 +0400

    Fixed file errors

--- scripts/eliminate/eliminate.py
+++ scripts/eliminate/eliminate.py
@@ -2,10 +2,10 @@ import subprocess
 import timeit
 import sys
 
-track_file = open("eliminated_logs_retry/eliminated_sites.txt", "a")
+track_file = open("eliminated_sites.txt", "a")
 
 # Determine offset
-offset_start_file = open("eliminated_logs_retry/eliminated_sites.txt", "r")
+offset_start_file = open("eliminated_sites.txt", "r")
 offset_start = len(offset_start_file.read().split("\n"))-1
 offset_start_file.close()
 
@@ -13,7 +13,7 @@ site_list = open("site_list", "r").read().split("\n")
 no_of_sites = len(site_list)-offset_start
 for in_i, i in enumerate(site_list[offset_start:]):
     url = i
-    init_url_file = open("/../../proxies/read_proxy_initiating_url", "w")
+    init_url_file = open("../../proxies/read_proxy_initiating_url", "w")
     init_url_file.write(url)
     init_url_file.close()
     print("Running DCE for ", in_i+1, "of", no_of_sites, ":", url)
